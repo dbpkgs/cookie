5fda38f6bb41ed40dafd9e851e1ea038a25cc7ca
sheldon-welinga <sheldonwelinga@gmail.com>
2023-05-28 17:19:33 +0300

    chore: add ignore script

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,11 @@
 import type { JestConfigWithTsJest } from 'ts-jest';
 
 const config: JestConfigWithTsJest = {
-  transform: {
-    '^.+\\.(t|j)sx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  preset: 'ts-jest',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(tsx?)$',
   testEnvironment: 'jest-environment-jsdom',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/lib/'],
 };
 
 export default config;
