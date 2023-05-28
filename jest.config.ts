7fab15486616d2ee58d1762c386bcc058bce11b0
sheldon-welinga <sheldonwelinga@gmail.com>
2023-05-28 17:23:30 +0300

    chore: update the present and esm

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,8 @@
 import type { JestConfigWithTsJest } from 'ts-jest';
 
 const config: JestConfigWithTsJest = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(tsx?)$',
   testEnvironment: 'jest-environment-jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
