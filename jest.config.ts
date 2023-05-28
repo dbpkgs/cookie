0a36f850fdd4bc2382a84f1dcd9a4bf4f25e406a
sheldon-welinga <sheldonwelinga@gmail.com>
2023-05-28 17:48:03 +0300

    chore: update jest config and add coverage report

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,7 @@
 import type { JestConfigWithTsJest } from 'ts-jest';
 
 const config: JestConfigWithTsJest = {
-  preset: 'ts-jest/presets/default-esm',
-  extensionsToTreatAsEsm: ['.ts'],
+  preset: 'ts-jest',
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(tsx?)$',
   testEnvironment: 'jest-environment-jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
